a27e9fcb517ae2f373ffe262d34b67483a0c5fe4
carzh <wolfivyaura@gmail.com>
2024-12-03 21:38:38 -0800

    added day 1

---
+++ day01a.cc
@@ -0,0 +1,46 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
+
+// run g++ -o day01a day01a.cc
+// then run the executable
+
+int main(int argc, char* argv[]) {
+    std::string filePath = "inputs/day01a";
+
+    std::ifstream file(filePath);
+
+    std::vector<int> list1;
+    std::vector<int> list2;
+
+    std::string line;
+    while(std::getline(file, line)) {
+        std::istringstream lineStream(line);
+
+        int num1, num2;
+
+        if (lineStream >> num1 >> num2) {
+            list1.push_back(num1);
+            list2.push_back(num2);
+        }
+    }
+
+    file.close();
+
+    std::sort(list1.begin(), list1.end());
+    std::sort(list2.begin(), list2.end());
+
+    int diff = 0;
+
+    for (int i = 0; i < list1.size(); i++) {
+        diff += std::abs(list1[i] - list2[i]);
+    }
+
+    std::cout << "CALCULATED DIFFERENCE: " << diff << std::endl;
+
+    return 0;
+}
---
+++ day01b.cc
@@ -0,0 +1,50 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
+#include <unordered_map>
+
+// run g++ -o day01b day01b.cc
+// then run the executable
+
+int main(int argc, char* argv[]) {
+    std::string filePath = "inputs/day01a";
+
+    std::ifstream file(filePath);
+
+    std::vector<int> list1;
+    std::unordered_map<int, int> list2Counts;
+
+    std::string line;
+    while(std::getline(file, line)) {
+        std::istringstream lineStream(line);
+
+        int num1, num2;
+
+        if (lineStream >> num1 >> num2) {
+            list1.push_back(num1);
+            if (list2Counts.find(num2) != list2Counts.end()) {
+                list2Counts[num2] += 1;
+            } else {
+                list2Counts[num2] = 1;
+            }
+        }
+    }
+
+    file.close();
+
+    int similarity = 0;
+
+    for (const int list1num : list1) {
+        if (list2Counts.find(list1num) != list2Counts.end()) {
+            similarity += (list1num * list2Counts[list1num]);
+        }
+    }
+
+    std::cout << "CALCULATED SIMILARITY: " << similarity << std::endl;
+
+    return 0;
+}
